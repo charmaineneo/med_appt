17c914899fa8f448a329c6cfa1f2e3b8e7d69d32
charmaineneo <cnsm2000@hotmail.com>
2024-10-29 08:19:05 -0400

    doctors

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Sign_Up from './Components/Sign_up/Sign_Up';
 import Login from './Components/Login/Login';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
 
 // Function component for the main App
 function App() {
@@ -27,6 +28,7 @@ function App() {
             <Route path="/" element={<Landing_Page/>}/>
             <Route path="/signup" element={<Sign_Up/>}/>
             <Route path="/login" element={<Login/>}/>
+            <Route path="/instant-consultation" element={<InstantConsultation />} />
           </Routes>
         </BrowserRouter>
     </div>
